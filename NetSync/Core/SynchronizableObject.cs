5caae7f1bfb1d98c5f9623b0ea8cd49b2198b729
Markus Andree <me@hapm.de>
2014-01-06 00:02:44 +0100

    changed tabs into spaces

--- NetSync/Core/SynchronizableObject.cs
+++ NetSync/Core/SynchronizableObject.cs
@@ -19,44 +19,66 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 namespace NetSync.Core
 {
-	using System;
-	using System.Runtime.Serialization;
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
 
-	/// <summary>
-	/// A Syncronizable object can be synchronized over network.
-	/// </summary>
-	[Serializable]
-	public class SynchronizableObject : GenericPropertyCollection
-	{
-		private ISource source;
-		private Uri uri;
-		
-		public SynchronizableObject(ISource source, Uri uri) : base()
-		{
-			if (source == null)
-				throw new ArgumentNullException("source");
-			if (uri == null)
-				throw new ArgumentNullException("objectId");
-			
-			this.source = source;
-			this.uri = uri;
-			base.Object = this;
-		}
-		
-		public ISource Source { get { return source; } }
-		
-		public Uri Uri { get { return uri; } }
-		
-		public override DateTime LastUpdated {
-			get {
-				DateTime lastUpdate = DateTime.Now;
-				foreach (IProperty p in this) {
-					if (p.LastUpdated < lastUpdate)
-						lastUpdate = p.LastUpdated;
-				}
-				
-				return lastUpdate;
-			}
-		}
-	}
+    /// <summary>
+    /// A Syncronizable object can be synchronized over network.
+    /// </summary>
+    [Serializable]
+    public class SynchronizableObject : GenericPropertyCollection
+    {
+        private ISource source;
+        private Uri uri;
+        private Dictionary<string, IProperty> dynamicProperties;
+        
+        public SynchronizableObject(ISource source, Uri uri) : base()
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (uri == null)
+                throw new ArgumentNullException("objectId");
+            
+            this.dynamicProperties = new Dictionary<string, IProperty>();
+            this.source = source;
+            this.uri = uri;
+            base.Object = this;
+        }
+        
+        public ISource Source { get { return source; } }
+        
+        public Uri Uri { get { return uri; } }
+        
+        public override DateTime LastUpdated {
+            get {
+                DateTime lastUpdate = DateTime.Now;
+                foreach (IProperty p in this) {
+                    if (p.LastUpdated < lastUpdate)
+                        lastUpdate = p.LastUpdated;
+                }
+                
+                return lastUpdate;
+            }
+        }
+        
+        public override IProperty this[string id] {
+            get {
+                IProperty property = base[id];
+                if (property != null)
+                    return property;
+                
+                return dynamicProperties[id];
+            }
+        }
+        
+        public void SetProperty(string name, IProperty property) {
+            if (name.Contains("."))
+                throw new ArgumentException("Dots aren't allowed in a property name.", "name");
+            if (base[name] != null)
+                throw new ArgumentException("Given name is a fixed property of a SynchronizableObject", "name");
+            dynamicProperties.Remove(name);
+            dynamicProperties.Add(name, property);
+        }
+    }
 }
